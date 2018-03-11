af75f7a96f65ab7176675e8c12f7bc25820a15fb
BKertesz <kertesz.balazs92@gmail.com>
2018-03-11 17:11:46 +0000

    Added display functions

--- interface.rb
+++ interface.rb
@@ -1,5 +1,13 @@
 # Interface Module
 # Used to display game info
-def print_test_line()
-  p 'This is a test line'
+def display_menu()
+  return 'Welcome to the Chicken Farm Simulator'
+end
+
+def get_player_input()
+  commands = ['work','sleep','buy','sell']
+  p "Type in a command of following:#{commands}"
+  choice = gets.chomp()
+  return nil if !commands.include?(choice)
+  return choice
 end
--- specs/test_cases_02_interface.rb
+++ specs/test_cases_02_interface.rb
@@ -4,13 +4,44 @@ require_relative '../interface.rb'
 class TestInterface < Minitest::Test
 
   def setup()
-    @menu = {
-      welcome:'Welcome to the Chicken Simulator',
-      input:'What would you like to do?'
-      border:'@'
+    @player = {
+      name:"Pawel",
+      money:0,
+      inventory:[],
+      stamina:0
     }
-    @data = {}
+
+    @farm = {
+      animals: [],
+      wood:0,
+      stone:0,
+      hay:0
+    }
+
+    @chicken = {
+      name:'Betty',
+      age:1,
+      eggs:2,
+      price:50
+    }
+    @menu = {
+      welcome:'Welcome to the Chicken Farm Simulator'
+    }
+
   end
+
+# Tests if menu is displayed
+def test_display_menu()
+  assert_equal(@menu[:welcome],display_menu())
+end
+
+def test_player_input()
+  assert_equal('work',get_player_input())
+end
+
+def test_player_input__inputs_wrong_command()
+  assert_nil(get_player_input())
+end
 
 end
 
